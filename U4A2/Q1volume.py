23094934a10a3b6e672197014ba558d850f1be6f
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-12 15:18:16 +0000

    U4A2 - Q1Volume

---
+++ U4A2/Q1volume.py
@@ -0,0 +1,32 @@
+# NAME OF AUTHOR: Ariana Hrlic
+# NAME OF THE PROGRAM:Volume of Rectanuglar Prism
+# DATE OF CREATION: January, 12, 2022
+# PURPOSE OF PROGRAM: Calculate the volume of a rectangular prism 
+
+
+# VARIABLE DEFINITION
+
+length = 0
+width = 0
+height = 0 
+
+volume = 0 
+
+
+# INPUT
+
+length = int(input("please enter the length:"))
+
+width = int(input("please enter the width:"))
+
+height = int(input("please enter the height"))
+
+
+# PROCESSING
+
+volume = (length * width * height)
+
+
+
+# OUTPUT
+print(volume)

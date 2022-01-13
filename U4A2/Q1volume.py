d1c31c6816dcd1821e2abcc58dee6771a673d1da
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-13 09:18:21 -0500

    Update Q1volume.py

--- U4A2/Q1volume.py
+++ U4A2/Q1volume.py
@@ -15,6 +15,7 @@ volume = 0
 
 # INPUT
 
+#Each prompts the user to enter the dimensions of the rectangular prism
 length = int(input("please enter the length:"))
 
 width = int(input("please enter the width:"))

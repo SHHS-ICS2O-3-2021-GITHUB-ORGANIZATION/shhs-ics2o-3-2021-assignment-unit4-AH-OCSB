debd7e89fca77b578fd0bdb7a7e079b009099eeb
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-17 12:40:37 +0000

    U4A3 - Q2randomrange

---
+++ U4A3/Q2randomrange.py
@@ -0,0 +1,27 @@
+# NAME OF AUTHOR: Ariana Hrlic 
+# NAME OF THE PROGRAM: Random Range  
+# DATE OF CREATION:  January,14,2022
+# PURPOSE OF PROGRAM: generate a random number based on the range that the user chooses  
+
+
+# VARIABLE DEFINITION
+import random
+
+number1 = 0
+number2 = 0
+
+ran = 0
+
+number = 0
+
+# INPUT
+
+ran = (input("Please enter a range: "))
+
+# PROCESSING
+
+number = random.randint(ran)
+
+# OUTPUT
+
+print(Number)

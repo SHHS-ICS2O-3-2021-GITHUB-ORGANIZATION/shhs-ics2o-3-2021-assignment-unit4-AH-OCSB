c3e0176890a90814dd3d1f58d93fff724751233e
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-18 14:38:53 +0000

    U4A4 - Q1counting

---
+++ U4A4/Q1counting.py
@@ -0,0 +1,35 @@
+# NAME OF AUTHOR: Ariana Hrlic
+# NAME OF THE PROGRAM: Counting
+# DATE OF CREATION:  January, 18,2022
+# PURPOSE OF PROGRAM:  Using the numbers given by the user, count up to one of the numbers and then do the same but counting down to zero
+
+
+# VARIABLE DEFINITION
+
+number = 0
+largeNumber = 0
+
+
+# INPUT
+
+number = int(input("Please give a number to count up by: "))
+largeNumber = int(input("please give a large number to start at: "))
+
+
+# PROCESSING
+
+number2(for i in range (0,largeNumber,number)):
+ if number2 == number:
+  print(number2)
+
+number3(for i in range(largeNumber,0,number)):
+ if number3 == 0: 
+  print(number3)
+
+
+
+# OUTPUT
+
+print(number2)
+
+print(number3)

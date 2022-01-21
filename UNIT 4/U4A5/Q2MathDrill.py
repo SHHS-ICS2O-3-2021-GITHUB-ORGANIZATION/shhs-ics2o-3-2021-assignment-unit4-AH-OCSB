fbeccb063c917d7990d1790d9967f2c39a33ab51
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-21 18:27:38 +0000

    U4A5 - Q2 - Finished

---
+++ UNIT 4/U4A5/Q2MathDrill.py
@@ -0,0 +1,29 @@
+#Name of creator: Ariana Hrlic
+#Date of Creation: January,21,2022
+#Name of Creation: Math problem Generator
+#Purpuse of the program: Generate a multiplaction problem with random generated numbers
+
+import random 
+
+#variables
+number1 = 0
+number2 = 0
+question = 0
+answer = 0
+
+#input
+number1 = (random.randint(1, 12))
+number2 = (random.randint(1, 12))
+
+print(number1, number2)
+print("please answer the multiplaction question:")
+userAnswer = int(input())
+
+
+#output
+answer = int((number1 * number2))
+
+if userAnswer == answer: 
+  print("you are correct! ")
+elif userAnswer != answer:
+  print("Incorrect, you failed! The answer to this question was actually" + str(print(answer))

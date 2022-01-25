98b90ad63a6f90afe1dd21e715570b2e9cb9c558
AH-OCSB <97174106+AH-OCSB@users.noreply.github.com>
2022-01-25 13:58:19 +0000

    U4A6 - GuessNumberGame

--- UNIT 4/U4A6/Q1GuessingGame.py
+++ UNIT 4/U4A6/Q1GuessingGame.py
@@ -1,8 +1,8 @@
 #Name: Ariana Hrlic
-#Date of Creation: January 24, 2022
-#Name of program: Guess Number Game 
-#purpuse of program: allow user to chose a range and guess which number which was randomly selected
-#
+#Date of creation: January, 25, 2022
+#Name of program: Guess Number Game
+#Purpose of program: All user to guess number between a range they specify 
+
 import random
 
 #variables
@@ -11,35 +11,40 @@ userAnswer = 0
 minimunNumber = 0
 maximumNumber = 0
 answer = 0 
-counter = 0 
+counter = 0
 userAnswer = 0
 turn = 0
 
 #input
+#allows user to input their own range, minimum number and maximum number
 minimunNumber = int(input("please enter the minimum number you would like the random number to be between:"))
 
 maximumNumber = int(input("please enter the maximum number you would like the random number to be between:"))
 
-answer = random.randint(minimunNumber, maximumNumber) 
-
+answer = int(random.randint(minimunNumber, maximumNumber))
+#allow user to continue to guess untill they guess the number correctly 
 while turn == 0:
 
   userAnswer = int(input("please guess a number between the range that you had specified "))
 
+#output 
   if userAnswer == answer: 
-    print("You win! Youve guessed the number correctly")
-
-    turn = 1 
-
-#counts how many times it took the user and tells them
     counter = counter + 1
-    
+    print("You win! Youve guessed the number correctly")
     counter = (str(counter))
-    print(("You won! \n the numbers of tries you took was: ") + counter)
-    exit()
-    elif(userAnswer != answer)
-    print("You did not win :( ")
+    print(("You won! \n the numbers of tries you took was: ") +(counter))
+    turn = 1
+  else:
+      print("You did not win :( ")
+#counts how many times it took the user and tells them
+      counter = counter + 1 
 
-    counter = counter + 1 
+      print("try again!")
 
-    print("try again!")
+
+      turn = 0
+
+  
+    
+  
+    
